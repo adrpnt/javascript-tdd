e9f770e9104b17e8865844d8c9881a04cf248be2
Adriano Vasconcelos <adriano.vasconcelos@secaoweb.com.br>
2018-01-25 09:50:42 -0300

    calc example

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
-const a = 3;
-const b = 4;
+const sum = (num1, num2) => num1 + num2;
+const sub = (num1, num2) => num1 - num2;
+const mult = (num1, num2) => num1 * num2;
+const div = (num1, num2) => ((num2 === 0) ? 'Can not divide by zero!' : num1 / num2);
 
-// const c = 'constante C';
-
-function sum() {
-  return a + b;
-}
-
-sum();
+export { sum, sub, mult, div };
